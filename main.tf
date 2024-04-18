4d9535564579309b90f6d007211279b728e48e29
nrk19 <jcorreag@pm.me>
2024-04-18 23:18:42 +0200

    declaring dependencies between resource blocks

--- main.tf
+++ main.tf
@@ -1,31 +1,40 @@
 # file: main.tf
+
+# Providers installation
 terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "~> 5.0"  # Not update further than 5.x
     }
   }
 }
 
-provider "aws" {
-  region                   = "us-east-1" 
-  shared_credentials_files = ["${path.module}/key/aws_credentials"]
-}
-
-variable "instances_amount" {
-  type    = string
-  default = "3"   # Amount of instances to be deployed
+# Variables
+locals {
+  vars = {
+    ami              = "ami-0cd59ecaf368e5ccf"  # Ubuntu 20.04
+    instance_type    = "t3.small"               # 2vCPU | 2GiB Mem
+    instances_amount = "3"                      # Number of instances to be deployed
+    region           = "us-east-1"              
+  }
 }
 
 data "aws_vpc" "default" {
-  default = true  # Default VPC CIDR block
+  default = true  # Default VPC CIDR block (e.g: 172.31.10.0/16)
+}
+
+# Set AWS region and credentials
+provider "aws" {
+  region                   = local.vars.region
+  shared_credentials_files = ["${path.module}/key/aws_credentials"]
 }
 
 # Instances deployment
 resource "aws_instance" "main" {
-  ami           = "ami-0cd59ecaf368e5ccf"  # Ubuntu 20.04
-  instance_type = "t3.small"               # 2vCPU | 2GiB Mem
-  count         = var.instances_amount
+  ami           = local.vars.ami
+  count         = local.vars.instances_amount
+  instance_type = local.vars.instance_type
   key_name      = aws_key_pair.provisioner.key_name
   vpc_security_group_ids = [aws_security_group.allow_ssh_k8s.id]
   associate_public_ip_address = true
@@ -33,6 +42,11 @@ resource "aws_instance" "main" {
   tags = {  # The first instance to be deployed will be the k8s master
     Name = "${count.index == 0 ? "k8s_master" : "k8s_slave_${count.index - 1}"}"
   }
+
+  depends_on = [
+    aws_key_pair.provisioner,
+    aws_security_group.allow_ssh_k8s
+  ]
 }
 
 # Create SSH key pair for the provisioner (Ansible)
@@ -45,11 +59,10 @@ resource "aws_key_pair" "provisioner" {
 resource "aws_security_group" "allow_ssh_k8s" {
   name        = "allow_ssh_k8s"
   description = "Allow SSH and k8s inbound traffic and all outbound traffic"
-  # vpc_id      = aws_vpc.k8s_vpc.id
 
   # Inbound rules
   ingress {  # ssh
-    cidr_blocks       = ["0.0.0.0/0"]  # range of allowed IPs
+    cidr_blocks       = ["0.0.0.0/0"]  # Range of allowed IPs
     protocol          = "tcp"
     from_port         = 22
     to_port           = 22
@@ -63,7 +76,7 @@ resource "aws_security_group" "allow_ssh_k8s" {
   }
 
   ingress {  # kubectl
-    cidr_blocks       = [data.aws_vpc.default.cidr_block]  # Only allow communicatio between members of the default VPC
+    cidr_blocks       = [data.aws_vpc.default.cidr_block]  # Allow communication between k8s master and slaves
     protocol          = "tcp"
     from_port         = 6443
     to_port           = 6443
