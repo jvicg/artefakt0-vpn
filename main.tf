1ccafdc520adce4c3693a8618bcc11c49f4d0b86
nrk19 <jcorreag@pm.me>
2024-04-18 09:59:31 +0200

    adding vpc cidr block into a var to improve consistency

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 # file: main.tf
-
 terraform {
   required_providers {
     aws = {
@@ -13,100 +12,82 @@ provider "aws" {
   shared_credentials_files = ["${path.module}/key/aws_credentials"]
 }
 
-# instances
+variable "instances_amount" {
+  type    = string
+  default = "3"   # Amount of instances to be deployed
+}
+
+data "aws_vpc" "default" {
+  default = true  # Default VPC CIDR block
+}
+
+# Instances deployment
 resource "aws_instance" "main" {
-  ami           = "ami-0cd59ecaf368e5ccf"  # ubuntu 20.04
-  count         = "3"
-  instance_type = "t3.small"
+  ami           = "ami-0cd59ecaf368e5ccf"  # Ubuntu 20.04
+  instance_type = "t3.small"               # 2vCPU | 2GiB Mem
+  count         = var.instances_amount
   key_name      = aws_key_pair.provisioner.key_name
   vpc_security_group_ids = [aws_security_group.allow_ssh_k8s.id]
-  subnet_id     = aws_subnet.k8s_subnet.id
+  associate_public_ip_address = true
 
-  # network_interface {
-  #   network_interface_id = aws_network_interface.eth0.id
-  #   device_index         = 0
-  # }
-
-  tags = {
+  tags = {  # The first instance to be deployed will be the k8s master
     Name = "${count.index == 0 ? "k8s_master" : "k8s_slave_${count.index - 1}"}"
   }
 }
 
-# create a ssh key pair so ansible can access the instances
+# Create SSH key pair for the provisioner (Ansible)
 resource "aws_key_pair" "provisioner" {
   key_name   = "provisioner-key"
   public_key = file("${path.module}/key/provisioner.pub")
 }
 
-# create a subnet to make sure that instances are always running in the same network
-resource "aws_vpc" "k8s_vpc" {
-  cidr_block = "172.16.0.0/16"
-
-  tags = {
-    Name = "k8s-vpc"
-  }
-}
-
-resource "aws_subnet" "k8s_subnet" {
-  vpc_id            = aws_vpc.k8s_vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "k8s-subnet"
-  }
-}
-
-# security group (definition of firewall rules)
+# Security Group (definition of firewall rules)
 resource "aws_security_group" "allow_ssh_k8s" {
   name        = "allow_ssh_k8s"
-  description = "Allow SSH and K8s inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.k8s_vpc.id
+  description = "Allow SSH and k8s inbound traffic and all outbound traffic"
+  # vpc_id      = aws_vpc.k8s_vpc.id
 
-  # allow SSH incoming traffic
-  ingress {  
+  # Inbound rules
+  ingress {  # ssh
     cidr_blocks       = ["0.0.0.0/0"]  # range of allowed IPs
     protocol          = "tcp"
     from_port         = 22
     to_port           = 22
   }
 
-  # allow ICMP protocol (for testing porpuses)
-  ingress {  
+  ingress { # icmp (for testing porpuses) 
     cidr_blocks       = ["0.0.0.0/0"]
     protocol          = "icmp"
     from_port         = -1  # -1 stands for all ports
     to_port           = -1
   }
 
-  # allow connection between k8s master with the workers)
-  ingress {  
-    cidr_blocks       = ["172.16.10.0/24"]
+  ingress {  # kubectl
+    cidr_blocks       = [data.aws_vpc.default.cidr_block]  # Only allow communicatio between members of the default VPC
     protocol          = "tcp"
     from_port         = 6443
     to_port           = 6443
   }
 
-  # allow all the outgoing traffic
+  # Allow all the outgoing traffic
   egress {
-    # cidr_blocks       = ["0.0.0.0/0"]
+    cidr_blocks       = ["0.0.0.0/0"]
     protocol          = "-1" 
     from_port         = 0
     to_port           = 0
   }
 }
 
-# generate a file with the public DNS of the instances
-resource "local_file" "inventory" {
-  content = templatefile("${path.module}/template/ansible_inventory.tfpl", {
-    instances = aws_instance.main
+# Files generation
+resource "local_file" "inventory" {  # Ansible's inventory
+  content = templatefile("${path.module}/template/ansible_inventory.tftpl", {
+    instances = aws_instance.main,
   })
   filename = "${path.module}/build/provisioner/inventory"
 }
 
-# generate a file with the private IPs of the instances
-resource "local_file" "hosts" {
-  content = templatefile("${path.module}/template/k8s_hosts.tfpl", {
+resource "local_file" "hosts" {  # /etc/hosts (for the nodes)
+  content = templatefile("${path.module}/template/k8s_hosts.tftpl", {
     instances = aws_instance.main
   })
   filename = "${path.module}/build/provisioner/hosts"
