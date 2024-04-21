b4113238f4606324bfeeb215faffdfab1257f14e
nrk19 <jcorreag@pm.me>
2024-04-22 00:04:21 +0200

    improving error handling on entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,48 +3,39 @@
 # Script responsible of running Terraform and Ansible cancelling process if errors happens
 
 # Function to cancel running processes before exiting
-trap "cleanup" INT SIGINT SIGTERM
+trap cleanup SIGINT SIGTERM
 
 function cleanup() {
-    if [ -n "$TF_APPLY_PID" ]; then
-        kill -s SIGTERM "$TF_APPLY_PID"
+    if [ -n "$PROCESS_PID" ]; then
+        kill -s SIGTERM "$PROCESS_PID"
     fi
-
-    if [ -n "$ANSIBLE_PID" ]; then
-        kill -s SIGTERM "$ANSIBLE_PID"
-    fi
-
+ 
+    echo "exiting: Received SIGTERM. Terminating..."
 	exit 1
 }
 
-# Deploy the instances
-terraform init || exit $?
-terraform apply -auto-approve & 
-TERRAFORM_PID=$!
-wait $TERRAFORM_PID
+# Function to run a command handling errors
+function ex() {
+    "$@" &  
+    PROCESS_PID="$!"   
+    wait $PROCESS_PID
 
-if [ $? -ne 0 ]; then
-    exit $?
-fi
+    # Check for errors on command exit code
+    if [ $? -ne 0 ]; then
+        echo "error: Errors ocurred while running the command: '$*'"
+        exit $?
+    fi
 
-unset TERRAFORM_PID
+    unset PROCESS_PID
+}
 
-# Retrieve/update Ansible's inventory
-./venv/bin/python3 retrieve_inventory.py
-
-# Run Ansible
-./venv/bin/ansible-playbook roles/site.yml &
-ANSIBLE_PID=$!
-wait $ANSIBLE_PID
-
-if [ $? -ne 0 ]; then
-    exit $?
-fi
-
-unset ANSIBLE_PID
-
-# Main container loop
 main() {
+    ex terraform init                               # Initialize Terraform project
+    ex terraform apply -auto-approve                # Deploy the instances
+    ex ./venv/bin/python3 fetch_inventory.py        # Retrieve/update Ansible's inventory file
+    ex ./venv/bin/ansible-playbook roles/site.yml   # Run Ansible
+
+    # Main container loop
     while true; do
         sleep 1
     done
---
+++ fetch_inventory.py
@@ -0,0 +1,12 @@
+#!/bin/python
+# Script to retrieve Ansible's inventory file from the S3 bucket where it's storage
+
+import boto3  # type: ignore
+import sys
+
+try:
+    s3 = boto3.client("s3")
+    s3.download_file("provisioner-bucket", "inventory", "inventory")
+except Exception as e:
+    print("Error when retrieving the file:", e)
+    sys.exit(400)
