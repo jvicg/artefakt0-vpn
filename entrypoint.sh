0c3209246a22ef9fa305e8dd324a9ccade536e89
nrk19 <jcorreag@pm.me>
2024-04-22 08:53:32 +0200

    configuring entrypoint to receive user arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,12 @@
 trap cleanup SIGINT SIGTERM
 
 function cleanup() {
+    echo "warning: Received SIGTERM. Terminating..."
+
     if [ -n "$PROCESS_PID" ]; then
         kill -s SIGTERM "$PROCESS_PID"
     fi
  
-    echo "exiting: Received SIGTERM. Terminating..."
 	exit 1
 }
 
@@ -22,7 +23,7 @@ function ex() {
 
     # Check for errors on command exit code
     if [ $? -ne 0 ]; then
-        echo "error: Errors ocurred while running the command: '$*'"
+        echo "error: Errors ocurred while running the command: '$*'. Terminating..."
         exit $?
     fi
 
@@ -30,10 +31,17 @@ function ex() {
 }
 
 main() {
-    ex terraform init                               # Initialize Terraform project
-    ex terraform apply -auto-approve                # Deploy the instances
-    ex ./venv/bin/python3 fetch_inventory.py        # Retrieve/update Ansible's inventory file
-    ex ./venv/bin/ansible-playbook roles/site.yml   # Run Ansible
+
+    # Regular execution (no arguments received)
+    if [ "$#" -eq 0 ]; then
+        ex /usr/local/bin/terraform init                 # Initialize Terraform project
+        ex /usr/local/bin/terraform apply -auto-approve  # Deploy the instances
+        ex ./venv/bin/python3 fetch_inventory.py         # Retrieve/update Ansible's inventory file
+        ex ./venv/bin/ansible-playbook roles/site.yml    # Run Ansible
+    # If arguments received
+    else
+        ex "$@"
+    fi
 
     # Main container loop
     while true; do
